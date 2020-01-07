e6ee8ed78bd031f88a27ec4d7561dcbfdc51aae9
Nikita Lizogubov <nikitalizogubov2000@gmail.com>
2020-01-07 15:50:46 +0200

    init project

---
+++ MVP/MVP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVP
+//
+//  Created by Nikita Lizogubov on 07.01.2020.
+//  Copyright © 2020 Nikita Lizogubov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
